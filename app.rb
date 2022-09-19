fc22bc1b8ca7e5e0962eded0e9c4ca578c62f385
申思维 <shensiwei@sina.com>
2022-09-19 15:37:45 +0800

    增加了若干接口,使用了sinatra+json

--- app.rb
+++ app.rb
@@ -88,9 +88,81 @@ subdomain do
   end
 end
 
-# FIXME
 subdomain :api do
-  get '/name' do
+  get "/name" do
+    server_url = 'https://ensgraph.test-pns-link.com/subgraphs/name/graphprotocol/ens'
+    response = HTTParty.post server_url,
+      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'},
+      :body => {
+        "query": "query MyQuery {\n  domains(where: {name: \"#{params[:name]}\"}) {\n    id\n    labelName\n    name\n    resolver {\n      id\n    }\n    labelhash\n    owner {\n      id\n    }\n    parent {\n      id\n      labelName\n      labelhash\n      name\n    }\n    subdomainCount\n    subdomains {\n      id\n      labelName\n      labelhash\n      name\n    }\n    resolvedAddress {\n      id\n      domains {\n        labelName\n        labelhash\n        name\n      }\n    }\n    ttl\n  }\n}",
+        "variables": nil,
+        "operationName": "MyQuery",
+        "extensions":{"headers": nil}
+      }.to_json
+    puts "== response: #{response}"
+    body = JSON.parse(response.body)
+    domains = body['data']['domains'][0]
+    puts "==domains"
+    puts domains.inspect
+
+    response_registration = HTTParty.post server_url,
+      :headers => { 'content-type' => 'application/json', 'accept' => 'application/json'},
+      :body => {
+        "query": "query MyQuery {\n  registration(\n    id: \"#{domains['labelhash']}\"\n  ) {\n    id\n    expiryDate\n    labelName\n    registrationDate\n    cost\n  }\n}",
+        "variables": nil,
+        "operationname": "myquery",
+        "extensions":{"headers": nil}
+      }.to_json
+
+      puts "== response_registration: #{response_registration}"
+      body_registration = JSON.parse(response_registration.body)
+      puts body_registration['data'].inspect
+      registration = body_registration['data']['registration']
+      puts "==registration"
+      puts registration.inspect
+    json({
+      code: 1,
+      message: 'success',
+      result: {
+        name: "#{domains['name']}",
+        nameHash: "",
+        labelName: "#{domains['labelName']}",
+        labelhash: "#{domains['labelhash']}",
+        owner: {
+          id: "#{domains['owner']['id']}"
+        },
+        parent: {
+          id: "#{domains['parent']['id']}",
+          labelName: "#{domains['parent']['labelName']}",
+          labelhash: "#{domains['parent']['labelhash']}",
+          name: "#{domains['parent']['name']}"
+        },
+        subdomainCount: "#{domains['subdomainCount']}",
+        subdomains: {
+          id: "#{domains['subdomain']['id'] rescue ''}",
+          labelName: "#{domains['subdomain']['labelName'] rescue ''}",
+          labelhash: "#{domains['subdomain']['labelhash'] rescue '' }",
+          name: "#{domains['subdomain']['name'] rescue '' }",
+        },
+        resolvedAddress: {
+          id: "#{domains['resolvedAddress']['id']}",
+          domains: {
+            labelName: "#{domains['resolvedAddress']['domains'][0]['labelName'] rescue ''}",
+            labelhash: "#{domains['resolvedAddress']['domains'][0]['labelhash'] rescue ''}",
+            name: "#{domains['resolvedAddress']['domains'][0]['name'] rescue ''}"
+          }
+        },
+        ttl: "#{domains['ttl'] rescue ''}",
+        registration: {
+          id: "#{registration['id']}",
+          expiryDate: "#{registration['expiryDate']}",
+          labelName: "#{registration['labelName']}",
+          registrationDate: "#{registration['registrationDate']}",
+          cost: "#{registration['cost']}"
+        }
+      }
+    })
+
   end
 
   get '/reverse' do
