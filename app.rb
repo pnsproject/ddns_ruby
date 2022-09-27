a1be187f941bd4d2623ecb497933ef6b80d00cac
申思维 <shensiwei@sina.com>
2022-09-27 12:01:33 +0800

    修改了dot的api,完善了数据的抓取

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/json'
 require 'sinatra/subdomain'
 require 'httparty'
+require 'date'
 
 # 修改这个即可， 例如 ddns.so,  test-ddns.com
 #SITE_NAME = "test-ddns.com"
@@ -65,6 +66,12 @@ def get_ipfs_cid subdomain
   return result
 end
 
+# 根据 domain的名字，例如 vitalik.eth 获得对应的json
+def get_result_for_eth_name(name)
+end
+# 根据 domain的名字，例如 12345678.dot 获得对应的json
+def get_result_for_dot_name(name)
+end
 subdomain [:www, nil] do
   get '/' do
     json result: "Hi there~, subdomain is: #{subdomain}"
@@ -93,78 +100,147 @@ end
 
 subdomain :api do
   get "/name/:name" do
-    server_url = 'https://ensgraph.test-pns-link.com/subgraphs/name/graphprotocol/ens'
-    response = HTTParty.post server_url,
-      :headers => { 'Content-Type' => 'application/json', 'Accept' => 'application/json'},
-      :body => {
+    subdomain_type = params[:name].split('.').last
+    result = ''
+    case subdomain_type
+    when 'eth'
+      response = post_request server_url: 'https://ensgraph.test-pns-link.com/subgraphs/name/graphprotocol/ens',
+      body_in_hash: {
         "query": "query MyQuery {\n  domains(where: {name: \"#{params[:name]}\"}) {\n    id\n    labelName\n    name\n    resolver {\n      id\n    }\n    labelhash\n    owner {\n      id\n    }\n    parent {\n      id\n      labelName\n      labelhash\n      name\n    }\n    subdomainCount\n    subdomains {\n      id\n      labelName\n      labelhash\n      name\n    }\n    resolvedAddress {\n      id\n      domains {\n        labelName\n        labelhash\n        name\n      }\n    }\n    ttl\n  }\n}",
+        #"query": "query MyQuery {\n  domains(where: {name: \"#{params[:name]}\"}) {\n    id\n    labelName\n    name\n    resolver {\n      id\n      texts\n      contentHash\n      coinTypes\n      address\n    }\n    labelhash\n    owner {\n      id\n    }\n    subdomainCount\n    subdomains {\n      id\n      labelName\n      labelhash\n      name\n    }\n    resolvedAddress {\n      id\n      domains {\n        labelName\n        labelhash\n        name\n      }\n    }\n    ttl\n  }\n}\n",
         "variables": nil,
         "operationName": "MyQuery",
-        "extensions":{"headers": nil}
-      }.to_json
-    puts "== response: #{response}"
-    body = JSON.parse(response.body)
-    domains = body['data']['domains'][0]
-    puts "==domains"
-    puts domains.inspect
+      }
+      puts "== response: #{response}"
+      body = JSON.parse(response)
+      domains = body['data']['domains'][0]
+      puts "==domains"
+      puts domains.inspect
 
-    response_registration = HTTParty.post server_url,
-      :headers => { 'content-type' => 'application/json', 'accept' => 'application/json'},
-      :body => {
+      response_registration = post_request server_url: 'https://ensgraph.test-pns-link.com/subgraphs/name/graphprotocol/ens',
+      body_in_hash: {
         "query": "query MyQuery {\n  registration(\n    id: \"#{domains['labelhash']}\"\n  ) {\n    id\n    expiryDate\n    labelName\n    registrationDate\n    cost\n  }\n}",
         "variables": nil,
-        "operationname": "myquery",
-        "extensions":{"headers": nil}
-      }.to_json
+        "operationName": "MyQuery",
+      }
 
-      puts "== response_registration: #{response_registration}"
-      body_registration = JSON.parse(response_registration.body)
-      puts body_registration['data'].inspect
-      registration = body_registration['data']['registration']
-      puts "==registration"
-      puts registration.inspect
-    json({
-      code: 1,
-      message: 'success',
-      result: {
-        name: "#{domains['name']}",
-        nameHash: "",
-        labelName: "#{domains['labelName']}",
-        labelhash: "#{domains['labelhash']}",
-        owner: {
-          id: "#{domains['owner']['id']}"
-        },
-        parent: {
-          id: "#{domains['parent']['id']}",
-          labelName: "#{domains['parent']['labelName']}",
-          labelhash: "#{domains['parent']['labelhash']}",
-          name: "#{domains['parent']['name']}"
-        },
-        subdomainCount: "#{domains['subdomainCount']}",
-        subdomains: {
-          id: "#{domains['subdomain']['id'] rescue ''}",
-          labelName: "#{domains['subdomain']['labelName'] rescue ''}",
-          labelhash: "#{domains['subdomain']['labelhash'] rescue '' }",
-          name: "#{domains['subdomain']['name'] rescue '' }",
-        },
-        resolvedAddress: {
-          id: "#{domains['resolvedAddress']['id']}",
-          domains: {
-            labelName: "#{domains['resolvedAddress']['domains'][0]['labelName'] rescue ''}",
-            labelhash: "#{domains['resolvedAddress']['domains'][0]['labelhash'] rescue ''}",
-            name: "#{domains['resolvedAddress']['domains'][0]['name'] rescue ''}"
+        puts "== response_registration: #{response_registration}"
+        body_registration = JSON.parse(response_registration)
+        puts body_registration['data'].inspect
+        registration = body_registration['data']['registration']
+        puts "==registration"
+        puts registration.inspect
+      json({
+        code: 1,
+        message: 'success',
+        result: {
+          name: domains['name'],
+          nameHash: "",
+          labelName: domains['labelName'],
+          labelhash: domains['labelhash'],
+          owner: domains['owner']['id'],
+          parent: domains['parent']['id'],
+          subdomainCount: domains['subdomainCount'],
+          subdomains: {
+            id: "#{domains['subdomains']['id'] rescue ''}",
+            labelName: "#{domains['subdomains']['labelName'] rescue ''}",
+            labelhash: "#{domains['subdomains']['labelhash'] rescue ''}" ,
+            name: "#{domains['subdomains']['name'] rescue ''}",
+          },
+          resolvedAddress: {
+            id: domains['resolvedAddress']['id'],
+            domains: {
+              labelName: domains['resolvedAddress']['domains'][0]['labelName'],
+              labelhash: domains['resolvedAddress']['domains'][0]['labelhash'],
+              name: domains['resolvedAddress']['domains'][0]['name']
+            }
+          },
+          ttl: domains['ttl'],
+          cost: registration['cost'],
+          registration: {
+            id: registration['id'],
+            expiryDate: Time.at(registration['expiryDate'].to_i),
+            registrationDate: Time.at(registration['registrationDate'].to_i)
           }
-        },
-        ttl: "#{domains['ttl'] rescue ''}",
-        registration: {
-          id: "#{registration['id']}",
-          expiryDate: "#{registration['expiryDate']}",
-          labelName: "#{registration['labelName']}",
-          registrationDate: "#{registration['registrationDate']}",
-          cost: "#{registration['cost']}"
+        }
+      })
+
+    when 'dot'
+      temp_result = post_request server_url: 'https://moonbeamgraph.test-pns-link.com/subgraphs/name/graphprotocol/pns',
+      body_in_hash: {
+        "operationName": "MyQuery",
+        #"query": "query MyQuery {\n  domains(where: {labelName: \"#{params[:name]}\"}) {\n    labelhash\n    labelName\n    id\n    name\n    subdomains {\n      name\n      owner {\n        id\n      }\n      id\n      labelName\n      labelhash\n    }\n    subdomainCount\n    owner {\n      id\n    }\n    parent {\n      id\n    }\n  }\n  registrations(where: {labelName: \"#{params[:name]}\"}) {\n    expiryDate\n    events {\n      triggeredDate\n    }\n  }\n}\n",
+        "query": "query MyQuery {\n  domains(where: {labelName: \"#{params[:name].sub(".dot", '')}\"}) {\n    labelhash\n    labelName\n    id\n    name\n    subdomains {\n      id\n      name\n      labelName\n      labelhash\n    }\n    subdomainCount\n    owner {\n      id\n    }\n    parent {\n      id\n    }\n  }\n  sets(where: {domain_: {labelName: \"#{params[:name].sub(".dot", '')}\"}}) {\n    id\n    keyHash\n    value\n  }\n  registrations(where: {labelName: \"#{params[:name].sub(".dot", '')}\"}) {\n    expiryDate\n    events {\n      id\n      triggeredDate\n    }\n  }\n}\n",
+        "variables": nil
+      }
+
+      result_domain = JSON.parse(temp_result)['data']['domains'][0]
+      result_sets = JSON.parse(temp_result)['data']['sets']
+      result_registrations = JSON.parse(temp_result)['data']['registrations']
+      temp_hash = {
+        "DOT" => '70476024645083539914866120258902002044389822943217047784978736702069848167247',
+        "ETH" => '77201932000687051421874801696342701541816747065578039511607412978553675800564',
+        "BTC" => '105640063387051144792550451261497903460441457163918809975891088748950929433065',
+        "IPFS" => '109444936916467285377972213791356162468265265799777646334604004948560489512394',
+        "EMAIL" => '50101170924916254227885891120695131387383646459470231890507002477095918146885',
+        "NOTICE" => '31727182724036554852371956750201584211517824919105130426252222689897810866214',
+        "TWITTER_COM" => '11710898932869919534375710263824782355793106641910621555855312720536896315685',
+        "GITHUB" => '102576668688838416847107385580607409742813859881781246507337882384803237069874',
+        "TWITTER_URL" => '23368862207911262087635895660209245090921614897479706708279561601163163997039',
+        "AVATAR" => '98593787308120460448886304499976840768878166060614499815391824681489593998420',
+        "C_NAME" => '69611991539268867131500085835156593536513732089793432642972060827780580969128'
+      }
+      result_hash = {}
+      temp_hash.map { |key, value|
+        dot_value = []
+        result_sets.each { |e|
+          if e['keyHash'] == value
+            dot_value << e
+          end
+          if dot_value.last == nil
+            hash = result_hash.store(key, '')
+          else
+            hash = result_hash.store(key, dot_value.last)
+          end
         }
       }
-    })
+      puts "=== temp_hash : #{temp_hash}"
+      puts "=== result_hash: #{result_hash}"
+      # 取到了所有的 key hash 对应的value
+      result = {
+        name: result_domain['name'],
+        namehash: '',
+        labelName: result_domain['labelName'],
+        labelhash: result_domain['labelhash'],
+        owner: result_domain['owner']['id'],
+        parent: result_domain['parent']['id'],
+        expiryDate: Time.at(result_registrations[0]['expiryDate'].to_i),
+        registrationDate: Time.at(result_registrations[0]['events'].first['triggeredDate'].to_i),
+        subdomains: result_domain['subdomains'],
+        subdomainCount: result_domain['subdomainCount'],
+        records: {
+          DOT: result_hash['DOT']['value'],
+          ETH: result_hash['ETH']['value'],
+          BTC: result_hash['BTC']['value'],
+          IPFS: result_hash['IPFS']['value'],
+          Email: result_hash['EMAIL']['value'],
+          Notice: result_hash['NOTICE']['value'],
+          twitter: result_hash['TWITTER_COM']['value'],
+          github: result_hash['GITHUB']['value'],
+          Url: result_hash['TWITTER_URL']['value'],
+          Avatar: result_hash['AVATAR']['value'],
+          CNAME: result_hash['C_NAME']['value']
+        },
+      }
+
+      json({
+        code: 1,
+        message: 'success',
+        result: result
+      })
+    else
+      'only support .eth, .dot domain'
+    end
 
   end
 
@@ -199,7 +275,46 @@ subdomain :api do
       address: address,
       result: result
     })
+
   end
+  get '/domain/:subdomain' do
+    temp_result = post_request server_url: 'https://moonbeamgraph.test-pns-link.com/subgraphs/name/graphprotocol/pns',
+      body_in_hash: {
+        "operationName": "MyQuery",
+        "query": "query MyQuery {\n  domains(where: {name: \"zzzzzzzzzzzzzzzzzzzzz.dot\"}) {\n    labelhash\n    labelName\n    id\n    name\n    subdomainCount\n    owner {\n      id\n    }\n    parent {\n      id\n    }\n  }\n  sets(where: {domain_: {name: \"zzzzzzzzzzzzzzzzzzzzz.dot\"}}) {\n    id\n    keyHash\n    value\n  }\n  registrations {\n    expiryDate\n  }\n}",
+        "variables": nil
+      }
+
+    result_domain = JSON.parse(temp_result)['data']['domains'][0]
+    result_sets = JSON.parse(temp_result)['data']['sets'].last
+    result_registrations = JSON.parse(temp_result)['data']['registrations'].last
+    result = {
+      labelName: result_domain['labelName'],
+      labelhash: result_domain['labelhash'],
+      id: result_domain['id'],
+      name: result_domain['name'],
+      owner: {
+        id: result_domain['owner']['id']
+      },
+      parent: {
+        id: result_domain['parent']['id']
+      },
+      subdomainCount: result_domain['subdomainCount'],
+      records: {
+        "key - value": result_sets['id'],
+        keyHash: result_sets['keyHash'],
+        ipfs: result_sets['value']
+      },
+      expiryDate: result_registrations['expiryDate']
+    }
+
+    json({
+      code: 1,
+      message: 'success',
+      result: result
+    })
+  end
+
 end
 
 get '/' do
